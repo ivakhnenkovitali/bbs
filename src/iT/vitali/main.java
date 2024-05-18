7080135bcc7c9f1cec24d125859ddfb358a8daa7
Asus <vaxa2320@gmail.com>
2024-05-18 16:38:31 +0300

    01345

--- src/iT/vitali/main.java
+++ src/iT/vitali/main.java
@@ -2,7 +2,7 @@ package iT.vitali;
 
 public class main {
     public static void main(String[]arr){
-        System.out.println("hello vitali");
+        System.out.println("hello  vitali");
 
 
     }
